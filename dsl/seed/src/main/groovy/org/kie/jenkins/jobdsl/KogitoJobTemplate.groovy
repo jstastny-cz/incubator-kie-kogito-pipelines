947f3c5cc2dafa11d7208f23ea1076c1c6bd7f6a
jstastny-cz <jan.stastny@ibm.com>
2023-09-19 08:19:39 +0200

    t1

--- dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/KogitoJobTemplate.groovy
+++ dsl/seed/src/main/groovy/org/kie/jenkins/jobdsl/KogitoJobTemplate.groovy
@@ -502,13 +502,9 @@ class KogitoJobTemplate {
                     buildOriginPRMerge(true)
                 }
             }
-            properties {
-                envVars { 
-                    properties("""BRANCH=${Utils.getGitMainBranch(script)}
+            properties("""BRANCH=${Utils.getGitMainBranch(script)}
 GENERATION_BRANCH=${Utils.getGenerationBranch(script)}
 """)
-                }
-            }
             orphanedItemStrategy {
                 discardOldItems {
                     daysToKeep(10)
